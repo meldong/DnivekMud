10137a881d960fb12a5731426fbba145ac08dd34
Kevin Dong <meldong@outlook.com>
2023-06-08 17:20:18 +0800

    Add Minor Changes

--- PlaywithVuetify/src/App.vue
+++ PlaywithVuetify/src/App.vue
@@ -6,8 +6,11 @@
 
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
       <v-toolbar-title>Application</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
