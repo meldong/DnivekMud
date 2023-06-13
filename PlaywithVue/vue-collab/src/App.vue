07d8891ccae4d921b1fe79725bc3607cdc80bb4d
Kevin Dong <kevin.dong@asmpt.com>
2023-06-13 14:42:27 +0800

    Add Collab App

---
+++ PlaywithVue/vue-collab/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>Application</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn href="https://example.com" target="_blank" icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
