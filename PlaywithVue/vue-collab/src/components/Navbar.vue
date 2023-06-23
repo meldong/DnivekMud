32cfea034808beda14f873f464a96c5235f3f804
Kevin Dong <meldong@outlook.com>
2023-06-23 22:14:04 +0800

    Add Minor Changes

--- PlaywithVue/vue-collab/src/components/Navbar.vue
+++ PlaywithVue/vue-collab/src/components/Navbar.vue
@@ -1,6 +1,16 @@
 <template>
   <nav>
-    <v-toolbar flat dark app>hello</v-toolbar>
+    <v-toolbar flat app>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">todo</span>
+        <span>dnivek</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn plain color="grey">
+        <span>Sign Out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-toolbar>
   </nav>
 </template>
 
