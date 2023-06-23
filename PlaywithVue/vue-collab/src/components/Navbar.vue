dc7b08b28c58dd239c99bccee4881780198daca4
Kevin Dong <meldong@outlook.com>
2023-06-23 22:16:20 +0800

    Add Minor Changes

--- PlaywithVue/vue-collab/src/components/Navbar.vue
+++ PlaywithVue/vue-collab/src/components/Navbar.vue
@@ -2,8 +2,8 @@
   <nav>
     <v-toolbar flat app>
       <v-toolbar-title class="text-uppercase grey--text">
-        <span class="font-weight-light">todo</span>
-        <span>dnivek</span>
+        <span class="font-weight-light">d</span>
+        <span>nivek</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn plain color="grey">
