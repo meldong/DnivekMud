31637f459c453071c64bfcddc400ea7ab7041f95
Kevin Dong <meldong@outlook.com>
2023-06-24 11:57:20 +0800

    Add Minor Changes

--- PlaywithVue/vue-collab/src/components/Navbar.vue
+++ PlaywithVue/vue-collab/src/components/Navbar.vue
@@ -5,9 +5,9 @@
         class="grey--text"
         @click="drawer = !drawer"
       ></v-app-bar-nav-icon>
-      <v-toolbar-title class="grey--text">
-        <span class="text-lowercase font-weight-light">dr</span>
-        <span class="text-uppercase">dnivek</span>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">dr</span>
+        <span>nivek</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn plain color="grey">
@@ -16,8 +16,18 @@
       </v-btn>
     </v-toolbar>
 
-    <v-navigation-drawer v-model="drawer" app class="success">
-      <p>test</p>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -28,6 +38,11 @@ export default {
 
   data: () => ({
     drawer: false,
+    items: [
+      { title: "Home", icon: "mdi-home", to: "/" },
+      { title: "Apps", icon: "mdi-view-dashboard", to: "/task" },
+      { title: "About", icon: "mdi-help-box", to: "/about" },
+    ],
   }),
 };
 </script>
