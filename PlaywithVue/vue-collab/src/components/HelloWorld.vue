e2346155660b56ba9fae4912db5e6d906cdaa2cf
Kevin Dong <meldong@outlook.com>
2023-06-26 20:04:05 +0800

    Add Minor Changes

--- PlaywithVue/vue-collab/src/components/HelloWorld.vue
+++ PlaywithVue/vue-collab/src/components/HelloWorld.vue
@@ -44,6 +44,16 @@
           <v-card class="pa-2" outlined tile> One of three columns </v-card>
         </v-col>
       </v-row>
+      <v-row no-gutters>
+        <v-col cols="12" sm="6" md="8">
+          <v-card class="pa-2" outlined tile>
+            .col-12 .col-sm-6 .col-md-8
+          </v-card>
+        </v-col>
+        <v-col cols="6" md="4">
+          <v-card class="pa-2" outlined tile> .col-6 .col-md-4 </v-card>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
