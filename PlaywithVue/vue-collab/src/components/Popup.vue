e6166e0bbf32256523f02583b05b9dbd96b808a3
Kevin Dong <meldong@outlook.com>
2023-07-03 21:51:01 +0800

    Add Form

--- PlaywithVue/vue-collab/src/components/Popup.vue
+++ PlaywithVue/vue-collab/src/components/Popup.vue
@@ -1,32 +1,41 @@
 <template>
   <div class="popup">
-    <v-dialog v-model="dialog" width="500">
+    <v-dialog v-model="dialog" width="600">
       <template v-slot:activator="{ on, attrs }">
         <v-btn depressed rounded text v-bind="attrs" v-on="on">
-          Edit Account
+          Add New Project
         </v-btn>
       </template>
 
       <v-card>
         <v-card-title class="text-h5 grey lighten-2">
-          Privacy Policy
+          Add New Project
         </v-card-title>
 
         <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          <v-form ref="form">
+            <v-text-field
+              v-model="title"
+              :counter="20"
+              label="Title"
+              prepend-icon="mdi-folder"
+              required
+            ></v-text-field>
+
+            <v-textarea
+              v-model="content"
+              label="Information"
+              prepend-icon="mdi-pencil"
+              required
+            ></v-textarea>
+          </v-form>
         </v-card-text>
 
         <v-divider></v-divider>
 
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="primary" text @click="dialog = false"> I accept </v-btn>
+          <v-btn color="success" text @click="submit"> Add Project </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -39,6 +48,14 @@ export default {
 
   data: () => ({
     dialog: false,
+    title: "",
+    content: "",
   }),
+  methods: {
+    submit() {
+      console.log(this.title, this.content);
+      this.dialog = false;
+    },
+  },
 };
 </script>
