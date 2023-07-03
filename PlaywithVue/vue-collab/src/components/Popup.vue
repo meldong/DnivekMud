34917459a872def71ac9fb51a373678b80bce1ef
Kevin Dong <meldong@outlook.com>
2023-07-03 20:49:28 +0800

    Update Popup

--- PlaywithVue/vue-collab/src/components/Popup.vue
+++ PlaywithVue/vue-collab/src/components/Popup.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="popup">popup</div>
+  <div class="popup">
+    <v-dialog v-model="dialog" width="500">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn depressed rounded text v-bind="attrs" v-on="on">
+          Edit Account
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2">
+          Privacy Policy
+        </v-card-title>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="dialog = false"> I accept </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
@@ -7,7 +38,7 @@ export default {
   name: "Popup",
 
   data: () => ({
-    //
+    dialog: false,
   }),
 };
 </script>
