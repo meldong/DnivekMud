4f7335806d5541580b70af31d426f07145c9d757
Kevin Dong <meldong@outlook.com>
2023-07-08 18:09:50 +0800

    Add Minor Changes

---
+++ PlaywithJsonwebtoken/db.js
@@ -0,0 +1,8 @@
+const users = [
+  {
+    email: "meldong2@outlook.com",
+    password: "123456",
+  },
+];
+
+module.exports = { users };
--- PlaywithJsonwebtoken/index.js
+++ PlaywithJsonwebtoken/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const auth = require("./routes/auth");
+
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+app.use("/auth", auth);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hi, I am Dnivek!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Now running on port ${port}!`);
 });
+
+// User provides email and password
+// Validate email and password
+// Validate if user with that email does not exist
+// hash password
+// save user to db
+// send jwt
