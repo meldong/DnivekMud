4c348bc76d66a13b88b01f4629cf0fd77d448443
Kevin Dong <meldong@outlook.com>
2023-07-14 17:24:22 +0800

    Add Api Group

--- PlaywithJsonwebtoken/index.js
+++ PlaywithJsonwebtoken/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const auth = require("./routes/auth");
 const post = require("./routes/post");
-const user = require("./routes/user");
+const routes = require("./routes/routes");
 const logger = require("./logger/logger");
 
 const app = express();
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/auth", auth);
 app.use("/posts", post);
-app.use("/users", user);
+app.use("/api", routes);
 
 app.get("/", (req, res) => {
   res.send("Hi, I am Dnivek!");
--- PlaywithJsonwebtoken/routes/user.js
+++ PlaywithJsonwebtoken/routes/user.js
@@ -1,11 +1,8 @@
 const router = require("express").Router();
+
 const users = [{ firstName: "fnam1", lastName: "lnam1", userName: "uname1" }];
 
 router.get("/", (req, res) => {
-  res.send("user route working");
-});
-
-router.get("/all", (req, res) => {
   res.json(users);
 });
 
