8feff7510295f8e882763216fb0c7feead559ac7
Kevin Dong <meldong@outlook.com>
2023-07-18 16:39:52 +0800

    Remove Files

--- PlaywithPyJwt/script.py
+++
@@ -1,7 +0,0 @@
-import jwt
-
-encoded_jwt = jwt.encode({"some": "payload"}, "secret", algorithm="HS256")
-print(encoded_jwt)
-
-decoded = jwt.decode(encoded_jwt, "secret", algorithms=["HS256"])
-print(decoded['some'])
