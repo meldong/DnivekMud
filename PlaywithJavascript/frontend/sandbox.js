50a878139221196089653ef242ab171557b5e6fa
Kevin Dong <meldong@outlook.com>
2023-07-19 20:48:06 +0800

    Add Minor Changes

--- PlaywithJavascript/frontend/sandbox.js
+++ PlaywithJavascript/frontend/sandbox.js
@@ -1,1 +1,9 @@
-let age = 25;
+// =============================================================================
+// for loops
+// =============================================================================
+const names = ["shaun", "mario", "luigi"];
+
+for (let i = 0; i < names.length; i++) {
+  console.log("in loop:", i, names[i]);
+}
+console.log("loop finished");
