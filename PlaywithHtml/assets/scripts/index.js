2037d7e2c7b59396957754bdba176b3415f28290
Kevin Dong <kevin.dong@asmpt.com>
2023-10-18 11:47:37 +0800

    Add Minor Changes

---
+++ PlaywithHtml/assets/scripts/index.js
@@ -0,0 +1,14 @@
+// Performing a task
+function greet(firstName, lastName) {
+  console.log("Hello " + firstName + " " + lastName);
+}
+
+greet("Kevin", "D");
+
+// Calculating a value
+function square(number) {
+  return number * number;
+}
+
+let number = square(2);
+console.log(number);
