6cd78666fd87dfee3093f78843a1d8f67335e5a3
Kevin Dong <kevin.dong@asmpt.com>
2023-12-06 16:07:01 +0800

    Add Minor Changes

--- PlaywithJsonwebtoken/test-express-jwt/client/src/App.jsx
+++ PlaywithJsonwebtoken/test-express-jwt/client/src/App.jsx
@@ -6,6 +6,24 @@ function App() {
   const [user, setUser] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const axiosJWT = axios.create();
+
+  axiosJWT.interceptors.request.use(
+    async (config) => {
+      let currentDate = new Date();
+      const decodedToken = jwt_decode(user.accessToken);
+      if (decodedToken.exp * 1000 < currentDate.getTime()) {
+        const data = await refreshToken();
+        config.headers["authorization"] = "Bearer " + data.accessToken;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -17,9 +35,46 @@ function App() {
     }
   };
 
+  const handleDelete = async (id) => {
+    setSuccess(false);
+    setError(false);
+    try {
+      await axiosJWT.delete("/users/" + id, {
+        headers: { authorization: "Bearer " + user.accessToken },
+      });
+      setSuccess(true);
+    } catch (err) {
+      setError(true);
+    }
+  };
+
   return (
     <div className="container">
-      {user === null ? (
+      {user ? (
+        <div className="home">
+          <span>
+            Welcome to the <b>{user.isAdmin ? "admin" : "user"}</b> dashboard{" "}
+            <b>{user.username}</b>.
+          </span>
+          <span>Delete Users:</span>
+          <button className="deleteButton" onClick={() => handleDelete(1)}>
+            Delete John
+          </button>
+          <button className="deleteButton" onClick={() => handleDelete(2)}>
+            Delete Jane
+          </button>
+          {error && (
+            <span className="error">
+              You are not allowed to delete this user!
+            </span>
+          )}
+          {success && (
+            <span className="success">
+              User has been deleted successfully...
+            </span>
+          )}
+        </div>
+      ) : (
         <div className="login">
           <form onSubmit={handleSubmit}>
             <div className="formTitle">DESIGN MANAGER</div>
@@ -40,8 +95,6 @@ function App() {
             </button>
           </form>
         </div>
-      ) : (
-        <span>User has been logged in.</span>
       )}
     </div>
   );
